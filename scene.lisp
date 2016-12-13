a9c369b228336ee63608f602c4f4fe929252d3b6
Erik Peldan <epe@trifork.com>
2016-12-13 19:09:05 +0100

    Work on projection transforms

--- scene.lisp
+++ scene.lisp
@@ -17,6 +17,12 @@
   (list 0 1 0 0))
 
 
+(defun object-color (object)
+  "Return an object's primary color"
+  ;; TODO
+  (list 1 1 1))
+
+
 (defun use-object-coordinates (object)
   "Translate, rotate and scale coordinate space to use object coordinates"
   (let ((position (object-position object))
@@ -31,12 +37,31 @@
   (gl:with-pushed-matrix
     (use-object-coordinates object)
 
+    (apply #'gl:color (object-color object))
+    
     ;; TODO parameterize based on object type?
     (glut:wire-cube 0.5)))
 
 
 ;;
-;; Scene
+;;  Camera
+;; 
+
+
+(defun scene-camera (scene)
+  "Get the camera used by the scene"
+  :todo)
+
+
+(defun use-camera-projection (camera)
+  "Adapt the projection transform to reflect the given camera's view"
+  (gl:matrix-mode :projection)
+  (gl:load-identity)
+  (glu:perspective 60 (/ 640 320) 1 20) ; TODO figure out what we want here
+  ) 
+
+;;
+;;  Scene
 ;;
 
 (defun scene-objects (scene)
@@ -46,6 +71,9 @@
 
 (defun render (scene)
   "Render a full scene"
+
+  (use-camera-projection (scene-camera scene))
+  
   (gl:matrix-mode :modelview)
   (loop for obj in (scene-objects scene)
      do (render-object obj)))

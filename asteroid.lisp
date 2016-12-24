2dda62b573baeedfcad1f51827e3aa3b2c497272
Erik Peldan <epe@trifork.com>
2016-12-24 23:20:39 +0100

    Make ship triangle appear on screen

--- asteroid.lisp
+++ asteroid.lisp
@@ -29,35 +29,42 @@
 (defun notify-event (game event)
   "Notify the game that an input event happened")
 
-
 ;;
 ;;  Rendering
 ;; 
 (defun render-ship (ship)
   "Render a ship"
-  (destructuring-bind (x y) (ship-position ship)
-    ;; Setup
-    (gl:color 1.0 1.0 1.0)
-    (gl:translate x y 0)
-    (gl:rotate (ship-rotation ship) 0 0 1)
+  (the ship ship)
+  (with-slots (position rotation) ship
+    (let ((x (car position))
+          (y (cadr position)))
+      
+      ;; Setup
+      (gl:color 1.0 1.0 1.0)
+      (gl:translate x y 0)
+      (gl:rotate rotation 0 0 1)
+      (gl:scale 0.5 0.5 0)
 
-    ;; Draw
-    (gl:begin :line-loop)
-    (gl:vertex 0 -1)
-    (gl:vertex (sqrt 2) (sqrt 2))
-    (gl:vertex -(sqrt 2) (sqrt 2))
-    (gl:end)))
+      ;; Draw
+      (gl:begin :line-loop)
+      (gl:vertex 0 -1)
+      (gl:vertex (sqrt 2) (sqrt 2))
+      (gl:vertex (- (sqrt 2)) (sqrt 2))
+      (gl:end))))
 
 
 (defun render-game (game)
   "Render a game"
-  (gl:color 0 0 0))
+  (gl:color 0 0 0)
   (gl:clear :color-buffer)
   (with-slots (ship) game
-    (render-ship ship)))
+    (render-ship ship))
+  (glut:swap-buffers))
 
 
 (defun game-loop (&key (game (new-game)))
   "Enter a game loop running the specified game"
-  (window:event-loop :title "Asteroids"))
+  (window:event-loop :title "Asteroids"
+                     :renderer (lambda ()
+                                 (render-game game))))
 

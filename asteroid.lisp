2d659c1e5728dd0426237a9514409c0e4e2106af
Erik Peldan <epe@trifork.com>
2016-12-29 19:26:33 +0100

    Allow rotating ship

--- asteroid.lisp
+++ asteroid.lisp
@@ -14,12 +14,29 @@
   (:documentation "The entity representing the player's ship"))
 
 
+(defclass player-state ()
+  ((turning :type keyword
+            :accessor player-turning
+            :initform nil
+            :documentation "Indicates if the direction the player is turning")
+   (thrusting :type thrusting
+              :accessor player-thrusting
+              :initform nil
+              :documentation "Indicates if the player is trying to move forward"))
+  (:documentation "Represents information about a player (human or computer)"))
+
+
 (defclass asteroids-game ()
   ((ship :type ship
          :accessor asteroids-ship
          :initarg :ship
-         :documentation "The ship belonging to the player"))
+         :documentation "The ship belonging to the player")
+   
+   (player-state :type player-state
+                 :accessor game-player
+                 :initform (make-instance 'player-state)))
   (:documentation "A game of asteroids"))
+
 
 (defun new-game ()
   "Create a new game"
@@ -27,7 +44,23 @@
                  :ship (make-instance 'ship)))
 
 (defun notify-event (game event)
-  "Notify the game that an input event happened")
+  "Notify the game that an input event happened"
+  ;(format t "Event ~a. Rotation: ~a~%" event (ship-rotation (asteroids-ship game)))
+  (case (first event)
+    (:tick
+     t)
+    
+    (:keydown
+     (case (second event)
+       (#\a (setf (player-turning (game-player game))
+                  :left))
+
+       (#\d (setf (player-turning (game-player game))
+                  :right))))
+
+    (:keyup
+     (setf (player-turning (game-player game))
+           nil))))
 
 ;;
 ;;  Rendering
@@ -38,19 +71,27 @@
   (with-slots (position rotation) ship
     (let ((x (car position))
           (y (cadr position)))
+
+      (gl:push-matrix)
       
       ;; Setup
       (gl:color 1.0 1.0 1.0)
       (gl:translate x y 0)
-      (gl:rotate rotation 0 0 1)
       (gl:scale 0.5 0.5 0)
+      (gl:rotate rotation 0 0 -1)
 
       ;; Draw
       (gl:begin :line-loop)
       (gl:vertex 0 -1)
-      (gl:vertex (sqrt 2) (sqrt 2))
-      (gl:vertex (- (sqrt 2)) (sqrt 2))
-      (gl:end))))
+      (gl:vertex (/ 1 (sqrt 2)) (/ 1 (sqrt 2)))
+      (gl:vertex (- (/ 1 (sqrt 2))) (/ 1 (sqrt 2)))
+      (gl:end)
+
+      (gl:begin :points)
+      (gl:vertex 0 0)
+      (gl:end)
+
+      (gl:pop-matrix))))
 
 
 (defun render-game (game)
@@ -62,9 +103,41 @@
   (glut:swap-buffers))
 
 
+;;
+;; Update logic
+(defun update-game (game tstep)
+  "Step the game forward `tstep` units of time"
+  (let ((ship (asteroids-ship game)))
+    (with-slots (turning) (game-player game)
+      (case turning
+        (:left
+         (setf (ship-rotation ship)
+               (- (ship-rotation ship)
+                  (* 2 tstep pi))))
+
+        (:right
+         (setf (ship-rotation ship)
+               (+ (ship-rotation ship)
+                  (* 2 tstep pi))))))
+
+    ;; TODO move the ship forward here
+    (with-slots (thrusting) (game-player game)
+      :todo)))
+
+;;
+;;  Top Level API
+;; 
 (defun game-loop (&key (game (new-game)))
   "Enter a game loop running the specified game"
   (window:event-loop :title "Asteroids"
+                     :event-callback (lambda (event)
+                                       (notify-event game event))
                      :renderer (lambda ()
+                                 (update-game game 0.3)
                                  (render-game game))))
 
+
+;; TODO move rendering into asteroid-rendering.lisp
+;; TODO move event processing into asteroid-events.lisp
+
+(quote (game-loop))
